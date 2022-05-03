23c6672fa4d9ba2a2e49f997ed3e8a8f1a3740d5
Ashok013 <ashok.7.r.jag@gmail.com>
2022-05-03 11:47:05 +0530

    First Commit

---
+++ src/Components/Photo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Photo extends Component {
+  render() {
+    return (
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4roPj6XJ_7h72plZIyzFtb5ZLr6QAV307qg&usqp=CAU" alt="Spidey" width='500' />
+      </div>
+    )
+  }
+}
---
+++ src/Components/Text.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Text extends Component {
+  render() {
+    return (
+      <div className='txt'>
+         <h1>Spidey</h1>
+         <p>This is Spider man</p>
+         <div className='blk'><a href='https://www.google.com' target='_blank'>Click here</a></div>
+      </div>
+    )
+  }
+}
